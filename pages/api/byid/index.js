297fc2be99951a98d83eb78c3a8ce2922175374b
totaldekadens <angelica.moberg@hotmail.com>
2023-02-15 16:00:03 +0100

    Check if it works when hardcoded

--- pages/api/byid/index.js
+++ pages/api/byid/index.js
@@ -1,15 +1,11 @@
-import { storyblokInit, apiPlugin, getStoryblokApi } from "@storyblok/react";
+import { storyblokInit, apiPlugin } from "@storyblok/react";
 import axios from "axios";
 import StoryblokClient from "storyblok-js-client";
 import randomstring from "randomstring";
 
-// Updates ALL stories
+// Updates one story
 export default async function handler(req, res) {
   const { method } = req;
-
-  /*   if (!req.body) {
-    return res.status(400).json({ success: false, data: "Check body" });
-  } */
 
   // Connection Storyblok Content API
   storyblokInit({
@@ -31,7 +27,8 @@ export default async function handler(req, res) {
         const defaultLang = "en"; // Default language
         // Data to tobbe translate by storyId
         let { data } = await Storyblok.get(
-          `spaces/${req.body.space_id}/stories/${req.body.story_id}/export.json?lang_code=${defaultLang}&export_lang=true`
+          //`spaces/${req.body.space_id}/stories/${req.body.story_id}/export.json?lang_code=${defaultLang}&export_lang=true`
+          `spaces/196581/stories/258991850/export.json?lang_code=en&export_lang=true`
         );
 
         /////////////////////////////////////////////////////
@@ -52,7 +49,7 @@ export default async function handler(req, res) {
         // Mockup data. Represents "data" we get back from laravel
         const json = {
           "83266319-7946-4ecd-b2ed-a51b8c17c08b:all-articles:title":
-            "En story uppdaterad" + random, // Test data
+            "En story uppdaterad " + random, // Test data
           language: "es",
           page: "258991850",
           text_nodes: 0,
@@ -64,7 +61,8 @@ export default async function handler(req, res) {
 
         // Updates story with specific language
         axios({
-          url: `https://mapi.storyblok.com/v1/spaces/${req.body.space_id}/stories/${req.body.story_id}/import.json?lang_code=${json.language}`,
+          url: `https://mapi.storyblok.com/v1/spaces/196581/stories/258991850/import.json?lang_code=es`,
+          // url: `https://mapi.storyblok.com/v1/spaces/${req.body.space_id}/stories/${req.body.story_id}/import.json?lang_code=${json.language}`,
           method: "put",
           headers: {
             Authorization: process.env.storyblockOathToken,
