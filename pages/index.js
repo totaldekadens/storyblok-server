03c9983707de062bdbb5b2ffd069a5cfa259f3e0
totaldekadens <angelica.moberg@hotmail.com>
2023-02-15 13:56:07 +0100

    new structure

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ export default function Home() {
         },
         body: JSON.stringify(body),
       };
-      let response = await fetch("/api/storyblok", request);
+      let response = await fetch("/api/byid", request);
       let result = await response.json();
       console.log(result);
     } catch (err) {
