9bd629b3ba71f99daf448d41fd9c2168a95a31ac
totaldekadens <angelica.moberg@hotmail.com>
2023-02-16 13:55:02 +0100

    Added some comments

--- pages/api/byid/index.js
+++ pages/api/byid/index.js
@@ -1,4 +1,4 @@
-import { storyblokInit, apiPlugin } from "@storyblok/react";
+import { storyblokInit, apiPlugin, getStoryblokApi } from "@storyblok/react";
 import axios from "axios";
 import StoryblokClient from "storyblok-js-client";
 import randomstring from "randomstring";
@@ -10,7 +10,6 @@ Updates one story (Triggered by Storyblok when story gets published)
 */
 
 // Initializing the cors middleware
-// You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
 const cors = Cors({
   methods: ["POST", "GET", "HEAD"],
 });
@@ -53,28 +52,59 @@ export default async function handler(req, res) {
         }
 
         const defaultLang = "en"; // Default language
-        // Data to tobbe translate by storyId
+
+        // Todo: Filter language. Get spaceinfo (not working?) whyy??
+        console.log(req.body.space_id);
+        let { data2 } = await Storyblok.get("spaces/", {});
+        console.log(data2);
+
+        // GETs translateable object from Storyblok,
         let { data } = await Storyblok.get(
           `spaces/${req.body.space_id}/stories/${req.body.story_id}/export.json?lang_code=${defaultLang}&export_lang=true`
-          //`spaces/196581/stories/258991850/export.json?lang_code=en&export_lang=true`
         );
+        // Example output from "data":
+        /* 
+          {
+            "8a6e38d3-e1b8-4f2b-ad93-a7e4a7f0fcab:teaser:headline": "Services",
+            "90a1c1c7-3fe5-4179-bb78-d07b972ddf43:feature:name": "Number 1",
+            "040bf7d1-cc19-46bc-8f58-7840eab49f7d:feature:name": "Number 2",
+            "e118d0e2-ea24-4b9e-a428-42edda5a0e83:feature:name": "Number 3",
+            "bbc9d6b1-951c-4844-aa39-f0a4b7c1c01c:feature:name": "Number 544",
+            page: "258991852",
+            language: "en",
+            url: "services",
+            text_nodes: 0,
+          };
+          */
 
-        /////////////////////////////////////////////////////
-        // #3
-        // POST object to Tobbe translate/Laravel
-        //console.log({ data, id: req.body.story_id, spaceId:req.body.space_id , toLang: filteredLang });
+        // Todo:  POST object to Tobbe translate/Laravel
+        /*  
+         const body = {
+            id, // 258991850
+            space_id: req.body.space_id, // 196581
+            data, // se eaxample above
+            toLang: filteredLang, // [ { "code": "de", "name": "German" } ]
+          };
+          
+          const request = {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(body),
+          };
+          let response = await fetch("/urltotobbetranslateapi", request);
+          let result = await response.json();
+          console.log(result); 
+           */
 
-        // #4
-        // Receive Translated objects and POST to Storyblok for each country. Testing on one story.
-        //console.log({ data, id, spaceId, toLang: filteredLang });
-
-        // #5
+        // Receive Translated objects and POST to Storyblok for each country.
         //console.log({ data, id, spaceId, toLang: filteredLang });
 
         // generates random string to show the trigger works
         const random = randomstring.generate(7);
 
-        // Mockup data. Represents "data" we get back from laravel
+        // Mockup data. Represents "data" we get back from laravel.
         const json = {
           "83266319-7946-4ecd-b2ed-a51b8c17c08b:all-articles:title":
             "En story uppdaterad " + random, // Test data
@@ -89,7 +119,6 @@ export default async function handler(req, res) {
 
         // Updates story with specific language
         let response = axios({
-          //url: `https://mapi.storyblok.com/v1/spaces/196581/stories/258991850/import.json?lang_code=es`,
           url: `https://mapi.storyblok.com/v1/spaces/${req.body.space_id}/stories/${req.body.story_id}/import.json?lang_code=${json.language}`,
           method: "put",
           headers: {
--- pages/api/index.js
+++ pages/api/index.js
@@ -2,8 +2,11 @@ import { storyblokInit, apiPlugin, getStoryblokApi } from "@storyblok/react";
 import axios from "axios";
 import StoryblokClient from "storyblok-js-client";
 import randomstring from "randomstring";
+/* 
 
-// Updates ALL stories
+Updates ALL stories (Triggered by button)
+
+*/
 export default async function handler(req, res) {
   const { method } = req;
 
@@ -12,6 +15,7 @@ export default async function handler(req, res) {
     accessToken: process.env.storyblokApiToken,
     use: [apiPlugin],
   });
+
   // Connection Storyblok Management API
   const Storyblok = new StoryblokClient({
     oauthToken: process.env.storyblockOathToken,
@@ -20,8 +24,14 @@ export default async function handler(req, res) {
   switch (method) {
     case "POST":
       try {
+        if (!req.body) {
+          return res.status(400).json({ success: false, data: "Check body" });
+        }
+
         const storyblokApi = getStoryblokApi();
         const defaultLang = "en"; // Default language
+
+        // Todo: Filter languages
 
         // Get all stories
         let { data } = await storyblokApi.get(`cdn/stories/`, {
@@ -40,33 +50,61 @@ export default async function handler(req, res) {
         const storyIds = data.stories.map((story) => story.id);
 
         /* 
-      #1 Loops through all Ids, 
-      #2 GETs all translateable objects from Storyblok, 
-      #3 POSTs object to laravel/TobbeTranslate, 
-      #4 Receives a response from Laravel (Translated object), 
-      #5 POSTs translated object to Storyblok 
-     */
+        #1 Loops through all Ids, 
+        #2 GETs translateable object from Storyblok, 
+        #3 POSTs object to laravel/TobbeTranslate, 
+        #4 Receives a response from Laravel (Translated object), 
+        #5 POSTs translated object to Storyblok 
+        */
+
         // #1
         for (let i = 0; i < storyIds.length; i++) {
           let id = storyIds[i];
+
           // #2
           let { data } = await Storyblok.get(
             `spaces/${req.body.space_id}/stories/${id}/export.json?lang_code=${defaultLang}&export_lang=true`
           );
 
-          /////////////////////////////////////////////////////
-          // #3
-          // POST object to Tobbe translate/Laravel
-          //console.log({ data, id, spaceId, toLang: filteredLang });
+          // Example output from "data":
+          /* 
+          {
+            "8a6e38d3-e1b8-4f2b-ad93-a7e4a7f0fcab:teaser:headline": "Services",
+            "90a1c1c7-3fe5-4179-bb78-d07b972ddf43:feature:name": "Number 1",
+            "040bf7d1-cc19-46bc-8f58-7840eab49f7d:feature:name": "Number 2",
+            "e118d0e2-ea24-4b9e-a428-42edda5a0e83:feature:name": "Number 3",
+            "bbc9d6b1-951c-4844-aa39-f0a4b7c1c01c:feature:name": "Number 544",
+            page: "258991852",
+            language: "en",
+            url: "services",
+            text_nodes: 0,
+          };
+          */
+
+          // #3 Todo:  POST object to Tobbe translate/Laravel
+          /*  
+         const body = {
+            id, // 258991850
+            space_id: req.body.space_id, // 196581
+            data, // se eaxample above
+            toLang: filteredLang, // [ { "code": "de", "name": "German" } ]
+          };
+          
+          const request = {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(body),
+          };
+          let response = await fetch("/urltotobbetranslateapi", request);
+          let result = await response.json();
+          console.log(result); 
+           */
 
           // #4
-          // Receive Translated objects and POST to Storyblok for each country. Testing on one story.
-          //console.log({ data, id, spaceId, toLang: filteredLang });
-
+          // Todo:  Receive Translated objects and POST to Storyblok for each country. Testing on one story.
           if (id == 258991850) {
-            // #5
-            //console.log({ data, id, spaceId, toLang: filteredLang });
-
             // generates random string to show the trigger works
             const random = randomstring.generate(7);
 
@@ -94,7 +132,9 @@ export default async function handler(req, res) {
             });
           }
         }
-        res.status(200).json({ success: true, data: "Något blev published!" });
+        res
+          .status(200)
+          .json({ success: true, data: "All stories are updated" });
       } catch (error) {
         res.status(400).json({ success: false, message: error });
       }
